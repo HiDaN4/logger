859d5cdd2c7cc76d1a01162c1b20d18e87c125a2
HiDaN4 <hidan446@gmail.com>
2020-09-27 13:10:55 -0700

    - Bring back conditional import for file output with "dart.io" change

--- lib/logger.dart
+++ lib/logger.dart
@@ -20,7 +20,7 @@ export 'src/printers/prefix_printer.dart';
 
 export 'src/platform/platform.dart';
 
-export 'src/log_output.dart';
+export 'src/log_output.dart' if (dart.io) 'src/outputs/file_output.dart';
 
 export 'src/log_filter.dart';
 export 'src/log_output.dart';

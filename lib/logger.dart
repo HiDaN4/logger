b5a76064c61365dcb6195927a85f77783747b8c2
HiDaN4 <hidan446@gmail.com>
2020-09-27 11:00:34 -0700

    - Fix file output export

--- lib/logger.dart
+++ lib/logger.dart
@@ -10,6 +10,7 @@ export 'src/outputs/console_output.dart';
 export 'src/outputs/stream_output.dart';
 export 'src/outputs/memory_output.dart';
 export 'src/outputs/multi_output.dart';
+export 'src/outputs/file_output.dart';
 
 export 'src/printers/pretty_printer.dart';
 export 'src/printers/logfmt_printer.dart';
@@ -19,8 +20,7 @@ export 'src/printers/prefix_printer.dart';
 
 export 'src/platform/platform.dart';
 
-export 'src/log_output.dart'
-    if (dart.library.io) 'src/outputs/file_output.dart';
+export 'src/log_output.dart';
 
 export 'src/log_filter.dart';
 export 'src/log_output.dart';
